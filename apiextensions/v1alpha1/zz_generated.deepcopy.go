openshift/api

Add apiextensions group and CompatibilityRequirement
Co-authored-by: Christian Schlotter <cschlott@redhat.com>

apiextensions/v1alpha1/zz_generated.deepcopy.go
@@ -1,0 +1,262 @@
+//go:build !ignore_autogenerated
+// +build !ignore_autogenerated
+
+// Code generated by codegen. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+)
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *APIExcludedField) DeepCopyInto(out *APIExcludedField) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new APIExcludedField.
+func (in *APIExcludedField) DeepCopy() *APIExcludedField {
+	if in == nil {
+		return nil
+	}
+	out := new(APIExcludedField)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *APIVersions) DeepCopyInto(out *APIVersions) {
+	*out = *in
+	if in.Additional != nil {
+		in, out := &in.Additional, &out.Additional
+		*out = make([]APIVersionString, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new APIVersions.
+func (in *APIVersions) DeepCopy() *APIVersions {
+	if in == nil {
+		return nil
+	}
+	out := new(APIVersions)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CRDData) DeepCopyInto(out *CRDData) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CRDData.
+func (in *CRDData) DeepCopy() *CRDData {
+	if in == nil {
+		return nil
+	}
+	out := new(CRDData)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirement) DeepCopyInto(out *CompatibilityRequirement) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirement.
+func (in *CompatibilityRequirement) DeepCopy() *CompatibilityRequirement {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirement)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CompatibilityRequirement) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirementList) DeepCopyInto(out *CompatibilityRequirementList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]CompatibilityRequirement, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirementList.
+func (in *CompatibilityRequirementList) DeepCopy() *CompatibilityRequirementList {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirementList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CompatibilityRequirementList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirementSpec) DeepCopyInto(out *CompatibilityRequirementSpec) {
+	*out = *in
+	in.CompatibilitySchema.DeepCopyInto(&out.CompatibilitySchema)
+	out.CustomResourceDefinitionSchemaValidation = in.CustomResourceDefinitionSchemaValidation
+	in.ObjectSchemaValidation.DeepCopyInto(&out.ObjectSchemaValidation)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirementSpec.
+func (in *CompatibilityRequirementSpec) DeepCopy() *CompatibilityRequirementSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirementSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirementStatus) DeepCopyInto(out *CompatibilityRequirementStatus) {
+	*out = *in
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]v1.Condition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	out.ObservedCRD = in.ObservedCRD
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirementStatus.
+func (in *CompatibilityRequirementStatus) DeepCopy() *CompatibilityRequirementStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirementStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilitySchema) DeepCopyInto(out *CompatibilitySchema) {
+	*out = *in
+	out.CustomResourceDefinition = in.CustomResourceDefinition
+	in.RequiredVersions.DeepCopyInto(&out.RequiredVersions)
+	if in.ExcludedFields != nil {
+		in, out := &in.ExcludedFields, &out.ExcludedFields
+		*out = make([]APIExcludedField, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilitySchema.
+func (in *CompatibilitySchema) DeepCopy() *CompatibilitySchema {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilitySchema)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CustomResourceDefinitionSchemaValidation) DeepCopyInto(out *CustomResourceDefinitionSchemaValidation) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CustomResourceDefinitionSchemaValidation.
+func (in *CustomResourceDefinitionSchemaValidation) DeepCopy() *CustomResourceDefinitionSchemaValidation {
+	if in == nil {
+		return nil
+	}
+	out := new(CustomResourceDefinitionSchemaValidation)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *MatchCondition) DeepCopyInto(out *MatchCondition) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new MatchCondition.
+func (in *MatchCondition) DeepCopy() *MatchCondition {
+	if in == nil {
+		return nil
+	}
+	out := new(MatchCondition)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ObjectSchemaValidation) DeepCopyInto(out *ObjectSchemaValidation) {
+	*out = *in
+	in.NamespaceSelector.DeepCopyInto(&out.NamespaceSelector)
+	in.ObjectSelector.DeepCopyInto(&out.ObjectSelector)
+	if in.MatchConditions != nil {
+		in, out := &in.MatchConditions, &out.MatchConditions
+		*out = make([]MatchCondition, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ObjectSchemaValidation.
+func (in *ObjectSchemaValidation) DeepCopy() *ObjectSchemaValidation {
+	if in == nil {
+		return nil
+	}
+	out := new(ObjectSchemaValidation)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ObservedCRD) DeepCopyInto(out *ObservedCRD) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ObservedCRD.
+func (in *ObservedCRD) DeepCopy() *ObservedCRD {
+	if in == nil {
+		return nil
+	}
+	out := new(ObservedCRD)
+	in.DeepCopyInto(out)
+	return out
+}

chrischdi/openshift-api

apiextensions/v1alpha1/zz_generated.deepcopy.go
@@ -1,0 +1,254 @@
+//go:build !ignore_autogenerated
+// +build !ignore_autogenerated
+
+// Code generated by codegen. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+)
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *APIExcludedField) DeepCopyInto(out *APIExcludedField) {
+	*out = *in
+	if in.Versions != nil {
+		in, out := &in.Versions, &out.Versions
+		*out = make([]APIVersionString, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new APIExcludedField.
+func (in *APIExcludedField) DeepCopy() *APIExcludedField {
+	if in == nil {
+		return nil
+	}
+	out := new(APIExcludedField)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *APIVersions) DeepCopyInto(out *APIVersions) {
+	*out = *in
+	if in.AdditionalVersions != nil {
+		in, out := &in.AdditionalVersions, &out.AdditionalVersions
+		*out = make([]APIVersionString, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new APIVersions.
+func (in *APIVersions) DeepCopy() *APIVersions {
+	if in == nil {
+		return nil
+	}
+	out := new(APIVersions)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CRDData) DeepCopyInto(out *CRDData) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CRDData.
+func (in *CRDData) DeepCopy() *CRDData {
+	if in == nil {
+		return nil
+	}
+	out := new(CRDData)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirement) DeepCopyInto(out *CompatibilityRequirement) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirement.
+func (in *CompatibilityRequirement) DeepCopy() *CompatibilityRequirement {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirement)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CompatibilityRequirement) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirementList) DeepCopyInto(out *CompatibilityRequirementList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]CompatibilityRequirement, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirementList.
+func (in *CompatibilityRequirementList) DeepCopy() *CompatibilityRequirementList {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirementList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CompatibilityRequirementList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirementSpec) DeepCopyInto(out *CompatibilityRequirementSpec) {
+	*out = *in
+	in.CompatibilitySchema.DeepCopyInto(&out.CompatibilitySchema)
+	out.CustomResourceDefinitionSchemaValidation = in.CustomResourceDefinitionSchemaValidation
+	in.ObjectSchemaValidation.DeepCopyInto(&out.ObjectSchemaValidation)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirementSpec.
+func (in *CompatibilityRequirementSpec) DeepCopy() *CompatibilityRequirementSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirementSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilityRequirementStatus) DeepCopyInto(out *CompatibilityRequirementStatus) {
+	*out = *in
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]v1.Condition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	out.ObservedCRD = in.ObservedCRD
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilityRequirementStatus.
+func (in *CompatibilityRequirementStatus) DeepCopy() *CompatibilityRequirementStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilityRequirementStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CompatibilitySchema) DeepCopyInto(out *CompatibilitySchema) {
+	*out = *in
+	out.CustomResourceDefinition = in.CustomResourceDefinition
+	in.RequiredVersions.DeepCopyInto(&out.RequiredVersions)
+	if in.ExcludedFields != nil {
+		in, out := &in.ExcludedFields, &out.ExcludedFields
+		*out = make([]APIExcludedField, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CompatibilitySchema.
+func (in *CompatibilitySchema) DeepCopy() *CompatibilitySchema {
+	if in == nil {
+		return nil
+	}
+	out := new(CompatibilitySchema)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CustomResourceDefinitionSchemaValidation) DeepCopyInto(out *CustomResourceDefinitionSchemaValidation) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CustomResourceDefinitionSchemaValidation.
+func (in *CustomResourceDefinitionSchemaValidation) DeepCopy() *CustomResourceDefinitionSchemaValidation {
+	if in == nil {
+		return nil
+	}
+	out := new(CustomResourceDefinitionSchemaValidation)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ObjectSchemaValidation) DeepCopyInto(out *ObjectSchemaValidation) {
+	*out = *in
+	in.NamespaceSelector.DeepCopyInto(&out.NamespaceSelector)
+	in.ObjectSelector.DeepCopyInto(&out.ObjectSelector)
+	if in.MatchConditions != nil {
+		in, out := &in.MatchConditions, &out.MatchConditions
+		*out = make([]admissionregistrationv1.MatchCondition, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ObjectSchemaValidation.
+func (in *ObjectSchemaValidation) DeepCopy() *ObjectSchemaValidation {
+	if in == nil {
+		return nil
+	}
+	out := new(ObjectSchemaValidation)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ObservedCRD) DeepCopyInto(out *ObservedCRD) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ObservedCRD.
+func (in *ObservedCRD) DeepCopy() *ObservedCRD {
+	if in == nil {
+		return nil
+	}
+	out := new(ObservedCRD)
+	in.DeepCopyInto(out)
+	return out
+}